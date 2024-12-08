fc20c407b1b6d58638dee7e4747c65e82b8e2ca0
Morteza Nazari <namo352@gmail.com>
2024-12-08 13:31:23 +0330

    add animation to faqs_screen.dart

--- lib/features/setting/presentation/screen/faqs_screen.dart
+++ lib/features/setting/presentation/screen/faqs_screen.dart
@@ -38,21 +38,18 @@ class _FaqsScreen extends State<FaqsScreen> {
         centerTitle: true,
         title: Text('FAQs '),
       ),
-      body: SingleChildScrollView(
-        child: SectionBox(
-          padding: EdgeInsets.all(10),
-          children: [
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: widget.faqList.length,
-              itemBuilder: (context, index) {
-                return FaqItem(
-                  question: widget.faqList[index].question,
-                  answer: widget.faqList[index].answer,
-                );
-              },
-            ),
-          ],
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: ListView.builder(
+          physics: ClampingScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: widget.faqList.length,
+          itemBuilder: (context, index) {
+            return FaqItem(
+              question: widget.faqList[index].question,
+              answer: widget.faqList[index].answer,
+            );
+          },
         ),
       ),
     );
@@ -73,68 +70,119 @@ class FaqItem extends StatefulWidget {
   State<FaqItem> createState() => _FaqItemState();
 }
 
-class _FaqItemState extends State<FaqItem> {
+class _FaqItemState extends State<FaqItem> with TickerProviderStateMixin {
+  late AnimationController _slideController;
+  late AnimationController _fadeController;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _fadeAnimation;
+
   bool showAnswer = false;
 
   @override
+  void initState() {
+    super.initState();
+
+    _slideController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    _slideAnimation = Tween<Offset>(
+      begin: Offset(0, -0.5),
+      end: Offset(0, 0),
+    ).animate(
+      CurvedAnimation(parent: _slideController, curve: Curves.easeInOut),
+    );
+
+    _fadeController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 600),
+    );
+    _fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(parent: _fadeController, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _slideController.dispose();
+    _fadeController.dispose();
+    super.dispose();
+  }
+
+  void toggleAnswer() {
+    setState(() {
+      showAnswer = !showAnswer;
+      if (showAnswer) {
+        _slideController.forward();
+        _fadeController.forward();
+      } else {
+        _slideController.reverse();
+        _fadeController.reverse();
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Ink(
-            padding: const EdgeInsets.symmetric(vertical: 10.0),
-            decoration: BoxDecoration(
-              border: Border.all(color: AppConst.color3),
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-            child: Column(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Ink(
+        padding: const EdgeInsets.symmetric(vertical: 10.0),
+        decoration: BoxDecoration(
+          border: Border.all(color: AppConst.color3),
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: Column(
+          children: [
+            Row(
               children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 10),
-                        child: Text(
-                          widget.question,
-                          maxLines: 10,
-                          softWrap: false,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: AppConst.color6),
-                        ),
-                      ),
-                    ),
-                    InkWell(
-                      borderRadius: BorderRadius.circular(100),
-                      onTap: () {
-                        setState(() {
-                          showAnswer = !showAnswer;
-                        });
-                      },
-                      child: Icon(
-                        !showAnswer ? Icons.add : Icons.remove,
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Text(
+                      widget.question,
+                      maxLines: 10,
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
                         color: AppConst.color6,
                       ),
                     ),
-                    SizedBox(width: 10)
-                  ],
+                  ),
                 ),
-                if (showAnswer)
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      widget.answer,
-                      //textAlign: TextAlign.start,
-                      style: TextStyle(color: AppConst.color5),
-                    ),
-                  )
+                InkWell(
+                  borderRadius: BorderRadius.circular(100),
+                  onTap: toggleAnswer,
+                  child: Icon(
+                    !showAnswer ? Icons.add : Icons.remove,
+                    color: AppConst.color6,
+                  ),
+                ),
+                SizedBox(width: 10),
               ],
             ),
-          ),
+            showAnswer
+                ? FadeTransition(
+                    opacity: _fadeAnimation,
+                    child: SlideTransition(
+                      position: _slideAnimation,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          widget.answer,
+                          style: TextStyle(color: AppConst.color5),
+                        ),
+                      ),
+                    ),
+                  )
+                : SizedBox.shrink(),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
